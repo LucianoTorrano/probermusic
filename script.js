a877dd85258accd32d6e3428f4e9a7205013a899
Luciano Torrano <lucianotorrano.dev@gmail.com>
2022-06-09 20:40:28 -0300

    responsive design added

--- script.js
+++ script.js
@@ -22,4 +22,41 @@ var icon = document.getElementById("sound");
 		}
 
 	}
+//Burger button animation
 
+const menuRespBtn = document.querySelector('.menuResponsive-btn');
+const navActive = document.querySelector('.nav-options');
+const navItemList = document.querySelectorAll('.nav-options__item');
+const navLinks = document.querySelectorAll('.nav-options__item a');
+
+menuRespBtn.onclick = ()=>{
+	//menu responsive deployment
+	menuRespBtn.classList.toggle('active');
+	navActive.classList.toggle('nav-active');
+
+	//menu links fade animation
+	linkFade(navItemList);
+
+}
+
+navItemList.forEach(link =>{
+	link.onclick = ()=>{
+		if(menuRespBtn.classList.contains('active') && navActive.classList.contains('nav-active')){
+			setTimeout(()=>{
+			navActive.classList.toggle('nav-active');
+			menuRespBtn.classList.toggle('active');
+			linkFade(navItemList)},200);
+		}
+	}
+	
+})
+
+function linkFade(linkList){
+	linkList.forEach((link,index)=>{
+		if(link.style.animation){
+			link.style.animation = '';
+		} else {
+			link.style.animation = `navLinkFade 0.5s ease forwards ${index / 4 + .5}s`
+		}
+	})
+}
